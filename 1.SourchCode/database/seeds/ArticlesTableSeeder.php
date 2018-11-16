51582fd084dec08c8ab4eeadf529c7be05ab0edb
khoivinh282828 <khoivinh282828@gmail.com>
2018-11-16 17:07:03 +0700

    cms_laravel

--- 1.SourchCode/database/seeds/ArticlesTableSeeder.php
+++ 1.SourchCode/database/seeds/ArticlesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Articles;
 
 class ArticlesTableSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+    	
     }
 }
